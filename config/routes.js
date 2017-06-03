b211f5b233dc9a08fb23cd3980eae8442a133bcf
pavlo-berezin <pavlo.berezin@admins-MBP.Dlink>
2017-06-04 00:37:53 +0300

    Added filtration

--- config/routes.js
+++ config/routes.js
@@ -3,54 +3,62 @@ var controllers = require('../app/controllers');
 var ArticleModel = models.ArticleModel;
 var categoriesController = controllers.categoriesController;
 module.exports = function (app) {
-  app.use(function(req, res, next) {
-    res.header("Content-Type", "application/json");
-    next();
-  });
+    app.use(function(req, res, next) {
+        res.header("Content-Type", "application/json");
+        next();
+    });
 
-  // Article routes
-  app.post('/article', function(req, res) {
-      var article = new ArticleModel(req.body);
-      article.save(function (err) {
-        if (!err) {
-            categoriesController.getEntities(article.body).then(function(resp){
+    // Article routes
+    app.post('/article', function(req, res) {
+        var article = new ArticleModel(req.body);
+        article.save(function (err) {
+            if (!err) {
+                categoriesController.getEntities(article.body).then(function(resp){
                 ArticleModel.update({ _id: article.id }, { $set: { categories: categoriesController.getCategories(resp)}}, function(article) {})
-            }).catch(function(error) {
-                console.log('failed to get Enitites for Article: ' + article.id);
-                console.log(error);
-            });
-            return res.send({ status: 'OK', article: article, message: 'HELLO'});
-        } else {
-            if(err.name == 'ValidationError') {
-                res.statusCode = 400;
-                res.send({ error: 'Validation error' });
+                }).catch(function(error) {
+                    console.log('failed to get Enitites for Article: ' + article.id);
+                    console.log(error);
+                });
+                return res.send({ status: 'OK', article: article, message: 'HELLO'});
             } else {
-                res.statusCode = 500;
-                res.send({ error: 'Server error' });
+                if(err.name == 'ValidationError') {
+                    res.statusCode = 400;
+                    res.send({ error: 'Validation error' });
+                } else {
+                    res.statusCode = 500;
+                    res.send({ error: 'Server error' });
+                }
+            }
+        });
+    });
+
+    app.get('/article/:id', function (req, res) {
+        const id = req.params.id;
+        ArticleModel.findById(id, function (err, found) {
+            res.send({ status: 'OK', article: found })
+        });
+    });
+
+    app.get('/article', function(req,res) {
+        var categories = req.query.categories && req.query.categories.split(',');
+        console.log(categories);
+        var filter = {}
+        if (categories) {
+            filter.categories = {
+                $in: categories
             }
         }
+        ArticleModel.find(filter).sort('-date').exec(function(err, found) {
+            if(!err) {
+                res.send({ status: 'OK', articles: found })
+            }
+        });
     });
-  });
 
-  app.get('/article/:id', function (req, res) {
-    const id = req.params.id;
-    ArticleModel.findById(id, function (err, found) {
-      res.send({ status: 'OK', article: found })
+    app.delete('/article/:id', function (req, res) {
+        const id = req.params.id;
+        ArticleModel.findByIdAndRemove(id, function (err, found) {
+            res.send({ status: 'OK', removed: found })
+        });
     });
-  });
-
-  app.get('/article', function(req,res) {
-    ArticleModel.find({}).sort('-date').exec(function(err, found) {
-      if(!err) {
-        res.send({ status: 'OK', articles: found })
-      }
-    })
-  });
-
-  app.delete('/article/:id', function (req, res) {
-    const id = req.params.id;
-    ArticleModel.findByIdAndRemove(id, function (err, found) {
-      res.send({ status: 'OK', removed: found })
-    });
-  });
 };
